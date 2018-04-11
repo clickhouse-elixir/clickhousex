a461479c36efe198688e2179b4ba717ed936d4d7
santaux <santaux@gmail.com>
2018-04-11 15:40:28 +0300

    Increased version to 0.2.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Clickhousex.Mixfile do
   def project do
     [
       app: :clickhousex,
-      version: "0.2.1",
+      version: "0.2.2",
       elixir: "~> 1.5",
       deps: deps(),
       package: package(),
